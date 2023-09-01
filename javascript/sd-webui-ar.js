0eb43474f57d340e47d7af19582a74e70e027d04
Midnight Phantom <130857837+midnight-god-01@users.noreply.github.com>
2023-09-01 14:03:44 +0530

    Update sd-webui-ar.js

--- javascript/sd-webui-ar.js
+++ javascript/sd-webui-ar.js
@@ -1,20 +1,22 @@
-if ("undefined" === typeof ar_button_titles) {
-	ar_button_titles = {};
+const ar_button_titles = {
+  "Calc": "Show or hide the aspect ratio calculator",
+  "\u{21c5}": "Swap width and height values",
+  "\u2B07\ufe0f": "Get dimensions from txt2img/img2img sliders",
+  "\u{1f5bc}": "Get dimensions from image on the current img2img tab",
+  "Calculate Height": "Calculate new height based on source aspect ratio",
+  "Calculate Width": "Calculate new width based on source aspect ratio",
+  "Apply": "Apply calculated width and height to txt2img/img2img sliders"
+};
+
+function setTooltips() {
+  const buttons = document.querySelectorAll('#txt2img_container_aspect_ratio button, #img2img_container_aspect_ratio button');
+  buttons.forEach(elem => {
+    const tooltip = ar_button_titles[elem.textContent];
+    if (tooltip) {
+      elem.title = tooltip;
+    }
+  });
 }
 
-ar_button_titles["Calc"] = "Show or hide the aspect ratio calculator";
-ar_button_titles["\u{21c5}"] = "Swap width and height values";
-ar_button_titles["\u2B07\ufe0f"] = "Get dimensions from txt2img/img2img sliders";
-ar_button_titles["\u{1f5bc}"] = "Get dimensions from image on current img2img tab";
-ar_button_titles["Calculate Height"] = "Calculate new height based on source aspect ratio";
-ar_button_titles["Calculate Width"] = "Calculate new width based on source aspect ratio";
-ar_button_titles["Apply"] = "Apply calculated width and height to txt2img/img2img sliders";
-
-onUiUpdate(function(){
-	gradioApp().querySelectorAll('#txt2img_container_aspect_ratio button, #img2img_container_aspect_ratio button').forEach(function(elem){
-		tooltip = ar_button_titles[elem.textContent];
-		if(tooltip){
-		 	elem.title = tooltip;
-		}
-	})
-})
+// Check for updates when the UI is updated
+onUiUpdate(setTooltips);
