0e7acf36a0f0004171ba9284df7393f086ecafed
Midnight Phantom <130857837+midnight-god-01@users.noreply.github.com>
2023-09-01 14:04:36 +0530

    Update button_titles.js

--- javascript/button_titles.js
+++ javascript/button_titles.js
@@ -1,2 +1,12 @@
-// Do not put custom titles here. This file is overwritten each time the WebUI is started.
-ar_button_titles = {}
+// This object stores tooltips for buttons in the WebUI.
+const ar_button_titles = {
+  "Calc": "Show or hide the aspect ratio calculator",
+  "\u{21c5}": "Swap width and height values",
+  "\u2B07\ufe0f": "Get dimensions from txt2img/img2img sliders",
+  "\u{1f5bc}": "Get dimensions from the image on the current img2img tab",
+  "Calculate Height": "Calculate new height based on the source aspect ratio",
+  "Calculate Width": "Calculate new width based on the source aspect ratio",
+  "Apply": "Apply calculated width and height to txt2img/img2img sliders"
+};
+
+// Do not modify or add custom titles here, as this file may be overwritten.
