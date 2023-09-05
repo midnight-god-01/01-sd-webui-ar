d37bb7a657707d9ccc57e21dc0e6041f501608b1
Midnight Phantom <130857837+midnight-god-01@users.noreply.github.com>
2023-09-05 19:36:28 +0530

    Update sd-webui-ar.js

--- javascript/sd-webui-ar.js
+++ javascript/sd-webui-ar.js
@@ -1,22 +1,27 @@
-const ar_button_titles = {
-  "Calc": "Show or hide the aspect ratio calculator",
-  "\u{21c5}": "Swap width and height values",
-  "\u2B07\ufe0f": "Get dimensions from txt2img/img2img sliders",
-  "\u{1f5bc}": "Get dimensions from image on the current img2img tab",
-  "Calculate Height": "Calculate new height based on source aspect ratio",
-  "Calculate Width": "Calculate new width based on source aspect ratio",
-  "Apply": "Apply calculated width and height to txt2img/img2img sliders"
-};
-
-function setTooltips() {
-  const buttons = document.querySelectorAll('#txt2img_container_aspect_ratio button, #img2img_container_aspect_ratio button');
-  buttons.forEach(elem => {
-    const tooltip = ar_button_titles[elem.textContent];
-    if (tooltip) {
-      elem.title = tooltip;
-    }
-  });
+// Check if arsp__ar_button_titles is undefined and initialize it as an empty object
+if (typeof arsp__ar_button_titles === "undefined") {
+    arsp__ar_button_titles = {};
 }
 
-// Check for updates when the UI is updated
-onUiUpdate(setTooltips);
+// Define button titles using Unicode characters and descriptions
+arsp__ar_button_titles["Calc"] = "Show or hide the aspect ratio calculator";
+arsp__ar_button_titles["\u{21c5}"] = "Swap width and height values";
+arsp__ar_button_titles["\u2B07\ufe0f"] = "Get dimensions from txt2img/img2img sliders";
+arsp__ar_button_titles["\u{1f5bc}"] = "Get dimensions from image on the current img2img tab";
+arsp__ar_button_titles["Calculate Height"] = "Calculate new height based on source aspect ratio";
+arsp__ar_button_titles["Calculate Width"] = "Calculate new width based on source aspect ratio";
+arsp__ar_button_titles["Apply"] = "Apply calculated width and height to txt2img/img2img sliders";
+arsp__ar_button_titles["\uD83D\uDD0D"] = "Round dimensions to the nearest multiples of 4 (1023x101 => 1024x100)";
+
+// Function to assign tooltips to buttons
+function assignTooltipsToButtons() {
+    document.querySelectorAll('#arsp__txt2img_container_aspect_ratio button, #arsp__img2img_container_aspect_ratio button').forEach(function (elem) {
+        const tooltip = arsp__ar_button_titles[elem.textContent];
+        if (tooltip) {
+            elem.title = tooltip;
+        }
+    });
+}
+
+// Call the function when the UI is updated
+onUiUpdate(assignTooltipsToButtons);
