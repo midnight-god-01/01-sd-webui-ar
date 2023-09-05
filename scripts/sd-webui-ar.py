664e4f28ed286afb960ee52737a79fc48110abc8
Midnight Phantom <130857837+midnight-god-01@users.noreply.github.com>
2023-09-05 20:25:09 +0530

    Update sd-webui-ar.py

--- scripts/sd-webui-ar.py
+++ scripts/sd-webui-ar.py
@@ -63,7 +63,7 @@ def parse_aspect_ratios_file(filename):
     labels, values, comments = [], [], []
     file = Path(BASE_PATH, filename)
 
-    if not file exists():
+    if not file.exists():
         return labels, values, comments
 
     with open(file, "r", encoding="utf-8") as f:
@@ -91,7 +91,7 @@ def parse_resolutions_file(filename):
     labels, values, comments = [], [], []
     file = Path(BASE_PATH, filename)
 
-    if not file exists():
+    if not file.exists():
         return labels, values, comments
 
     with open(file, "r", encoding="utf-8") as f:
